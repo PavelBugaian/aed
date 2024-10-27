bcba63157dba8b4dcada6d52ddc6682706c909d1
Pavel Bugaian <bugaian.pavel@gmail.com>
2024-10-27 23:07:08 +0200

    feat: wip, added bop and bop2 datasets, 4 indices are ready

--- Lab2/PCA.R
+++ Lab2/PCA.R
@@ -6,56 +6,41 @@ library(ggfortify)
 library(psych)
 
 gdp_score_tf <- t(gdp_rate_2)
-years <- as.vector(gdp_rate_2[1,-1])
 colnames(gdp_score_tf) <- gdp_score_tf[1,]
-gdp_score_tf <- as.data.frame(gdp_score_tf[-1,])
-
-hist(gdp_rate_de_num, col = "red", 
-     main = "GDP Rate in Germany", xlab = "GDP Rate, %")
-
-de_scaled <- scale(gdp_rate_de_num)
-
-plot(
-  years, 
-  de_scaled,
-  type = "b",
-  xlab = "year", 
-  ylab = "GDP Rate, %",
-  pch = 19, col = "red", 
-  main = "GDP Rate in Germany",
-)
+gdp_score_df <- as.data.frame(gdp_score_tf[-1,])
+gdp_score_scaled <- scale(as.numeric(gdp_score_tf$Germany))
 
 hicp_tf <- hicp[, c(-1, -2, -3, -4, -5)]
-hicp_de <- as.vector(hicp_tf[2,-1])
-hicp_de_num <- as.numeric(hicp[2,-1])
+hicp_de_num <- as.numeric(hicp[2,-1])[c(-1,-2,-3,-4)]
 
-plot(
-  as.vector(hicp_tf[1,-1]), 
-  hicp_de,
-  type = "b",
-  xlab = "year", 
-  ylab = "GDP Rate, %",
-  pch = 19, col = "red", 
-  main = "HICP",
-)
-
-gdp_pca <- as.numeric(gdp_rate_de_reversed[-c(1:3)])
-
-pca <- principal(pca_df, nfactors = 2, rotate = "varimax")
-pairs.panels(pca_df, gap=0, pch=21)
-
-hpi_normal <- t(hpi)
+hpi_normal <- t(HPI)
 colnames(hpi_normal) <- hpi_normal[1,]
 hpi_normal <- as.data.frame(hpi_normal[-1,])
-
 hpi_normal_scaled <- scale(as.numeric(hpi_normal$Germany))
 
-length(hicp_de_num)
-length(gdp_pca)
+
+# Balance of payments by country
+bop_tf <- t(bop2)
+colnames(bop_tf) <- bop_tf[1,]
+bop_tf_df <- as.data.frame(bop_tf[-1,])[c(-1,-2,-3,-4),]
+bop_tf_scaled <- scale(as.numeric(bop_tf_df$Germany))
+
+# Export market shares (BPM6)
+
+
+
+
+length(de_scaled)
+length(gdp_score_scaled)
 length(hpi_normal_scaled)
+length(bop_tf_scaled)
 
 pca_df <- data.frame(
   hicp = hicp_de_num,
-  gdp_date = gdp_pca,
-  hpi = hpi_normal_scaled
+  gdp_rate = gdp_score_scaled,
+  hpi = hpi_normal_scaled,
+  bop = bop_tf_scaled
 )
+
+pca <- principal(data.matrix(pca_df), nfactors = 2)
+pairs.panels(pca_df, gap=0, pch=21)
